2a9b6187ef0f4a68864a4c1075bc6d12ae9b952d
Rhys Arkins <rhys@arkins.net>
2017-12-12 12:08:43 +0100

    Approve only renovate[bot] PRs

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = robot => {
   robot.on("pull_request.opened", async context => {
-    const params = context.repo();
-    params.number = context.payload.number;
-    params.event = "APPROVE";
-    return context.github.pullRequests.createReview(params);
+    if (context.payload.sender.login === "renovate[bot]") {
+      const params = context.repo();
+      params.number = context.payload.number;
+      params.event = "APPROVE";
+      return context.github.pullRequests.createReview(params);
+    }
   });
 };
