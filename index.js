2b98dff13c8f03ba9d975c4ee37051f252038afd
Rhys Arkins <rhys@arkins.net>
2018-11-05 10:21:41 +0100

    Skip approving non-automerge PRs
    Fixes #9

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = robot => {
   robot.on("pull_request.opened", async context => {
-    if (context.payload.sender.login === "renovate[bot]") {
+    if (context.payload.sender.login === "renovate[bot]" && !context.payload.body.pull_request.body.includes("merge this manually") {
       const params = context.repo();
       params.number = context.payload.number;
       params.event = "APPROVE";
