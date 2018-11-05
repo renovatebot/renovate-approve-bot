723b7d569d45518e47f6cb8d80dcd4c0a0d05780
Rhys Arkins <rhys@arkins.net>
2018-11-05 10:54:23 +0100

    prettier

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 module.exports = robot => {
-  robot.on("pull_request.opened", async context => {
-    if (context.payload.sender.login === "renovate[bot]" && !context.payload.body.pull_request.body.includes("merge this manually")) {
+  robot.on('pull_request.opened', async context => {
+    if (
+      context.payload.sender.login === 'renovate[bot]' &&
+      !context.payload.body.pull_request.body.includes('merge this manually')
+    ) {
       const params = context.repo();
       params.number = context.payload.number;
-      params.event = "APPROVE";
+      params.event = 'APPROVE';
       return context.github.pullRequests.createReview(params);
     }
   });
