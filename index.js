09f6427db01d5edf5ab71cf247c70cb8de9526a9
Andrew Desmarais <terodox@gmail.com>
2019-09-15 13:31:43 -0400

    Add ability to handle dismissed PR reviews
    This change will allow a re-approval of a pull request that rebased by `renovate[bot]`.
    
    Re-approval requires the following conditions to be met:
    - The original pull request was opened by `renovate[bot]`
    - The approval being dismissed was created by `renovate-approve[bot]`
    - The dismissing commit was created by `renovate[bot]`
    - The pull request body does not contain the string `merge this manually` which indicates the PR can be merged automatically.
    
    I have been unable to validate this myself. It will need to be tested before being released. I have a high level of confidence that it will work as anticipated, but you can never be 100%!

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
+const APPROVE = 'APPROVE';
+const MANUAL_MERGE_MESSAGE = 'merge this manually';
+const RENOVATE_BOT = 'renovate[bot]';
+const RENOVATE_APPROVE_BOT = 'renovate-approve[bot]';
+ds
 module.exports = robot => {
   robot.log('App is loaded');
   robot.on('pull_request.opened', async context => {
     if (
-      context.payload.sender.login === 'renovate[bot]' &&
-      !context.payload.body.pull_request.body.includes('merge this manually')
+      context.payload.sender.login === RENOVATE_BOT &&
+      !context.payload.body.pull_request.body.includes(MANUAL_MERGE_MESSAGE)
     ) {
       const params = context.repo();
       params.number = context.payload.number;
-      params.event = 'APPROVE';
+      params.event = APPROVE;
       return context.github.pullRequests.createReview(params);
     }
+  });
+  app.on('pull_request_review.dismissed', async context => {
+    if (
+        context.payload.sender.login === RENOVATE_BOT &&
+        !context.payload.body.pull_request.body.includes(MANUAL_MERGE_MESSAGE) &&
+        context.payload.review.user.login === RENOVATE_APPROVE_BOT &&
+        context.payload.pull_request.user.login === RENOVATE_BOT
+      ) {
+        const params = context.repo();
+        params.number = context.payload.number;
+        params.event = APPROVE;
+        return context.github.pullRequests.createReview(params);
+      }
   });
 };
