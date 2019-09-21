1339a4207476a41430064e567a9f2c44830569ea
Rhys Arkins <rhys@arkins.net>
2019-09-21 18:57:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const APPROVE = 'APPROVE';
-const MANUAL_MERGE_MESSAGE = 'merge this manually';
-const RENOVATE_BOT = 'renovate[bot]';
-const RENOVATE_APPROVE_BOT = 'renovate-approve[bot]';
+const APPROVE = "APPROVE";
+const MANUAL_MERGE_MESSAGE = "merge this manually";
+const RENOVATE_BOT = "renovate[bot]";
+const RENOVATE_APPROVE_BOT = "renovate-approve[bot]";
 
-module.exports = robot => {
-  robot.log('App is loaded');
-  robot.on('pull_request.opened', async context => {
+module.exports = app => {
+  app.log("App is loaded");
+  app.on("pull_request.opened", async context => {
+    app.log("Received PR open event");
     if (
       context.payload.sender.login === RENOVATE_BOT &&
       !context.payload.body.pull_request.body.includes(MANUAL_MERGE_MESSAGE)
     ) {
+      app.log("Approving new PR");
       const params = context.repo();
       params.number = context.payload.number;
       params.event = APPROVE;
       return context.github.pullRequests.createReview(params);
     }
   });
-  robot.on('pull_request_review.dismissed', async context => {
+  app.on("pull_request_review.dismissed", async context => {
+    app.log("Received PR review dismiss event");
+    app.log(context.payload);
     if (
-        context.payload.sender.login === RENOVATE_BOT &&
-        !context.payload.body.pull_request.body.includes(MANUAL_MERGE_MESSAGE) &&
-        context.payload.review.user.login === RENOVATE_APPROVE_BOT &&
-        context.payload.pull_request.user.login === RENOVATE_BOT
-      ) {
-        const params = context.repo();
-        params.number = context.payload.number;
-        params.event = APPROVE;
-        return context.github.pullRequests.createReview(params);
-      }
+      context.payload.sender.login === RENOVATE_BOT &&
+      !context.payload.body.pull_request.body.includes(MANUAL_MERGE_MESSAGE) &&
+      context.payload.review.user.login === RENOVATE_APPROVE_BOT &&
+      context.payload.pull_request.user.login === RENOVATE_BOT
+    ) {
+      app.log("Re-approving dismissed approval");
+      const params = context.repo();
+      params.number = context.payload.number;
+      params.event = APPROVE;
+      return context.github.pullRequests.createReview(params);
+    }
   });
 };
