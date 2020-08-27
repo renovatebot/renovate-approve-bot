f3e871d3c2315f8de866cd4b1e37e32c8ba704af
Rhys Arkins <rhys@arkins.net>
2020-08-27 08:41:20 +0200

    refactor: use context.log where available

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ module.exports = app => {
     try {
       return context.payload.sender.login === RENOVATE_BOT;
     } catch (err) {
-      app.log(context.payload);
-      app.log(err);
+      context.log(context.payload);
+      context.log(err);
       return false;
     }
   }
@@ -19,8 +19,8 @@ module.exports = app => {
     try {
       return !context.payload.pull_request.body.includes(MANUAL_MERGE_MESSAGE);
     } catch (err) {
-      app.log(context.payload);
-      app.log(err);
+      context.log(context.payload);
+      context.log(err);
       return false;
     }
   }
@@ -28,7 +28,7 @@ module.exports = app => {
     try {
       return context.payload.review.user.login === RENOVATE_APPROVE_BOT;
     } catch (err) {
-      app.log(err);
+      context.log(err);
       return false;
     }
   }
@@ -36,8 +36,8 @@ module.exports = app => {
     try {
       return context.payload.pull_request.user.login === RENOVATE_BOT;
     } catch (err) {
-      app.log(context.payload);
-      app.log(err);
+      context.log(context.payload);
+      context.log(err);
       return false;
     }
   }
@@ -46,27 +46,27 @@ module.exports = app => {
       const params = context.issue({ event: APPROVE });
       return context.github.pulls.createReview(params);
     } catch (err) {
-      app.log(err);
-      app.log(context.payload);
+      context.log(err);
+      context.log(context.payload);
     }
   }
   app.on("pull_request.opened", async context => {
-    app.log("Received PR open event");
+    context.log("Received PR open event");
     if (isRenovate(context) && isAutomerging(context)) {
-      app.log("Approving new PR");
+      context.log("Approving new PR");
       return approvePr(context);
     }
   });
   app.on("pull_request_review.dismissed", async context => {
-    app.log("Received PR review dismiss event");
-    app.log(context.payload);
+    context.log("Received PR review dismiss event");
+    context.log(context.payload);
     if (
       isRenovate(context) &&
       isAutomerging(context) &&
       isRenovateApprover(context) &&
       isRenovateUser(context)
     ) {
-      app.log("Re-approving dismissed approval");
+      context.log("Re-approving dismissed approval");
       return approvePr(context);
     }
   });
